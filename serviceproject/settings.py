72126c99217205f14178bfcec973d492beae2358
unknown <mwangiarthur777@gmail.com>
2023-03-03 09:40:24 +0300

    my commit

--- serviceproject/settings.py
+++ serviceproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=$foj_c@2zw_(v+@@!u6un=hpqaj-*l*7m9*ql54ikt2!fm&%g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://web-production-8327.up.railway.app"]
 
 
 # Application definition
