480689b7cb4b2f6f14cc28a0064478c33c11cebd
unknown <mwangiarthur777@gmail.com>
2023-03-03 10:04:48 +0300

    new commit

--- serviceproject/settings.py
+++ serviceproject/settings.py
@@ -132,3 +132,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
+
+LOGIN_REDIRECT_URL = '/index/'
+LOGIN_URL = '/login/'
